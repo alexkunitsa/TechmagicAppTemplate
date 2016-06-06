f763f1d85c2713364e580617d3075f0362931823
Alex Kunitsa <alex.kunitsa@gmail.com>
2016-06-07 01:02:45 +0300

    Added Bridging Header

---
+++ TechmagicAppTemplate/TechmagicAppTemplate/Application/TechmagicAppTemplate-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TechmagicAppTemplate-Bridging-Header.h
+//  TechmagicAppTemplate
+//
+//  Created by Alex Kunitsa on 6/6/16.
+//  Copyright © 2016 TechMagic. All rights reserved.
+//
+
+#ifndef TechmagicAppTemplate_Bridging_Header_h
+#define TechmagicAppTemplate_Bridging_Header_h
+
+
+#endif /* TechmagicAppTemplate_Bridging_Header */
